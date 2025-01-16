760bbf20181b100d73339b0e65e501401aba6a09
WesleyTinkess <wtinkess7470@floridapoly.edu>
2025-01-16 16:51:35 +0000

    Implemented fourth program

---
+++ 04_get_input/get_input.c
@@ -0,0 +1,28 @@
+/*
+    Getting the input from the keyboard.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int square_side = 1;
+    int square_perimeter = 0;
+    int square_area = 0;
+
+    // Write the prompt
+    puts("Enter the side length of a square: ");
+
+    // Get the input
+    // & - address operator
+    scanf("%d", &square_side);
+
+    // Do the math
+    square_perimeter = square_side * 4;
+    square_area = square_side * square_side;
+
+    // Create the output
+    printf("The perimteter of a square with side length %d is %d.\n", square_side, square_perimeter);
+    printf("The area of a square with side length %d is %d.\n", square_side, square_area);
+
+    return 0;
+}
