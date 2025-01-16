67425b0361a03c16b1a778b9941a5ad76ef50602
WesleyTinkess <wtinkess7470@floridapoly.edu>
2025-01-16 16:26:44 +0000

    Implemented first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,18 @@
+// One-line comment
+// This is the first program which prints a welcoming message
+
+/*
+
+Multi-line comment
+January 9th 2025
+
+*/
+
+#include <stdio.h>
+// Create a main function
+int main() {
+    printf("Welcome to COP2271!\n");
+    printf("I want to code!\n");
+
+    return 0;
+}
