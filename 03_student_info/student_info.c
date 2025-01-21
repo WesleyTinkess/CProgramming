a22debdc3cd531f861b2fb50de3c9f6acd0ed774
WesleyTinkess <wtinkess7470@floridapoly.edu>
2025-01-21 16:04:28 +0000

    Implemented third program

---
+++ 03_student_info/student_info.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Create course and student IDs
+    int course_id = 2271;
+    int student_id = 10234;
+
+    // Print name
+    printf("Name: Wesley Tinkess\n");
+
+    // Print course and student IDs
+    printf("Course ID: %d\n", course_id);
+    printf("Student ID: %d\n", student_id);
+
+    // Print difference between student and course ID
+    printf("Student ID - Course ID is: %d\n", student_id - course_id);
+
+    return 0;
+}
